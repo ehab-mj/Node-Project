34e4ced0811c56376085220031da710dc68e9f6e
ehab-mj <ehabmj1212@gmail.com>
2024-03-20 23:03:38 +0200

    edit

--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,3 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
-// var app = require('../app');
-// var http = require('http');
 import debug from "debug";
 let log = debug("app:server");
 import env from "dotenv";
@@ -20,50 +12,32 @@ import {
   initialCards,
 } from "../initialData/initalDataService.js";
 
-/**
- * Get port from environment and store in Express.
- */
 
 let port = normalizePort(process.env.PORT || "3030");
 app.set("port", port);
 
-/**
- * Create HTTP server.
- */
-
 let server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
 
 function normalizePort(val) {
   let port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
   return false;
 }
 
-/**
- * Event listener for HTTP server "error" event.
- */
+
 
 function onError(error) {
   if (error.syscall !== "listen") {
@@ -71,8 +45,6 @@ function onError(error) {
   }
 
   let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
@@ -87,9 +59,6 @@ function onError(error) {
   }
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
 
 function onListening() {
   let addr = server.address();
